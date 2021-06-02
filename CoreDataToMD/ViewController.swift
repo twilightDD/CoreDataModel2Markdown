2c26a7437691d85bf9d71efd73a64603db3c92e6
Peter Hauke <peter@2sox.de>
2021-06-02 15:51:06 +0200

    Initial Commit

---
+++ CoreDataToMD/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  CoreDataToMD
+//
+//  Created by Peter Hauke on 02.06.21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
